8929e0a7b7483a55657526969994f9460410433d
shqhope <shqhope@foxmail.com>
2017-10-01 20:37:21 +0800

    commit ipc

---
+++ IPC/msgq/client/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "src/functions.h"
+
+using namespace std;
+
+int main(int argc, const char **argv)
+{
+	cout<< "hello world"<<endl;
+	int a = 100,b = 200;
+	cout << swap(&a, &b) << endl;
+	return 0;
+}
