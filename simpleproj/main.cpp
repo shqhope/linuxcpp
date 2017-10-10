5dc74c10ca5bb193c194f9dd6d8b47e5f74c28c1
sq@126.com <sq@126.com>
2017-10-10 10:45:03 +0800

    change main.cpp

--- simpleproj/main.cpp
+++ simpleproj/main.cpp
@@ -11,12 +11,13 @@ int main(int argc, const char **argv)
 	cout<< "hello world"<<endl;
 	int a = 100,b = 200;
 	cout << swap(&a, &b) << endl;
+	cout << time(0) << endl;
 	//LinkList::test();
 	//BTree::run();
-	Queen *queen = new Queen;
-	queen->Init(5);
-	queen->printQ();
-	int iframe = 0;
-	queen->GetFrame(0, iframe);
+	//Queen *queen = new Queen;
+	//queen->Init(5);
+	//queen->printQ();
+	//int iframe = 0;
+	//queen->GetFrame(0, iframe);
 	return 0;
 }
