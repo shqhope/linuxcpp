5c3a2a4f4c0b6ee58683457aeaed612617ae1c25
sq@126.com <sq@126.com>
2017-10-24 16:25:52 +0800

    add zwframework

---
+++ ZW/framework/zwquery.cpp
@@ -0,0 +1,13 @@
+#include "zwquery.h"
+
+typedef struct QUERYTHREADPARA
+{
+ //   ResultQueue *pResQueue;    //队列，保存查询结果
+ // QueryPara *pQueryPara;
+}QUERYTHREADPARA;
+
+void Query(QUERYTHREADPARA *pThreadPara)
+{
+//  开启多个结果集收集线程
+//  开启多个查询线程
+}
---
+++ ZW/framework/zwquery.h
@@ -0,0 +1,4 @@
+#ifndef ZWQUERY_H
+#define ZWQUERY_h
+
+#endif
